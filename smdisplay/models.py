2fd5b01e11a0448c851dce637873617e9786d5ab
Soumyajit <roy.valmont@gmail.com>
2021-11-08 20:57:18 +0530

    altered the price & image model

--- smdisplay/models.py
+++ smdisplay/models.py
@@ -20,7 +20,7 @@ class Customer(models.Model):
 
 class Product(models.Model):
     name= models.CharField(max_length=200, null=True)
-    price= models.FloatField()
+    price= models.DecimalField(max_digits=7, decimal_places= 2)
     digital= models.BooleanField(default=False, null=True, blank=True)
     description= models.TextField(default="Description")
     category= models.CharField(choices= PRODUCT_CHOICES, max_length=20, null=True)
